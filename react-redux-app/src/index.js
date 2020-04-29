2c1aed6c694569d5a78932b6cf77e469765cd2b3
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-28 21:56:24 -0400

    install react-redux

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore()
+// const store = createStore()
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  // <Provider store={store}>
+    <App />,
+  // </Provider>,
   document.getElementById('root')
 );
 
