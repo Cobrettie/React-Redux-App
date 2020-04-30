b6e4e0938c7b29a81d9b053db6b646b8cd912d45
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-29 20:34:05 -0400

    remove boilerplate JSX from App.js, build a component to test api connecting/requests, import Test component into App

--- react-redux-app/src/ContainerComponents/testing.js
+++ react-redux-app/src/ContainerComponents/testing.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-export default function Test() {
+function Test() {
   return (
     <div>
       <h2>testing.js component</h2>
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  console.log('Test component mSTP', state)
+  return {
+    state
+  }
+}
+
+export default connect(mapStateToProps, {})(Test)
